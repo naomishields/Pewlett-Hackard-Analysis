e6316729db388cca9ba9d7d19f9287358a8ad9c1
naomishields <58480113+naomishields@users.noreply.github.com>
2021-06-09 14:37:34 -0400

    Adding retirement_info

--- schema.sql
+++ schema.sql
@@ -42,3 +42,26 @@ CREATE TABLE salaries (
 );
 
 SELECT * FROM salaries;
+
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)	
+);
+
+SELECT * FROM dept_emp
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title)
+);
+
+SELECT * FROM titles
